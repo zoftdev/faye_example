278db0cc59b38c6a425630c752319d1c9d4b6114
Thisadee <hlex.java@gmail.com>
2014-05-05 11:38:57 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+J3::Application.config.secret_token = '3fb850371f0b68c85f7e8a45d6d4bf45254cdfee6d7bee132c754a020b5697ae1956a1b3cf213eb1c6c2d904b88dc46e6e38cfc697070a58812865889af1fa9f'
